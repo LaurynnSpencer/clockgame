169a0e2e63f97613109da312c40b38ee5b4b10fb
LaurynnSpencer <lauspe95@terpmail.umd.edu>
2025-10-25 18:46:58 -0400

    Commit1

---
+++ Assets/whatTimeIsIt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class whatTimeIsIt : MonoBehaviour
+{
+    [SerializeField] private const float htodeg = -30f, mtodeg = -6f, stodeg = -6f;
+    [SerializeField] Transform hpivot, mpivot, spivot;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hpivot.localRotation = Quaternion.Euler(-(htodeg * (float)time.TotalHours), 0f, 0f);
+        mpivot.localRotation = Quaternion.Euler(-(mtodeg * (float)time.TotalMinutes), 0f, 0f);
+        spivot.localRotation = Quaternion.Euler(-(stodeg * (float)time.TotalSeconds), 0f, 0f);
+    }
+}
